b5116a6a16da6e111b0be4a86ae0f4e6a9359bc7
Divyanshu Rawat <divyanshu.19r@gmail.com>
2020-11-26 22:57:47 +0530

    Refactored Service Interfaces. Closes #11

--- pet-clinic-data/src/main/java/com/rawatdivyanshu/petclinic/Services/VetService.java
+++ pet-clinic-data/src/main/java/com/rawatdivyanshu/petclinic/Services/VetService.java
@@ -2,13 +2,6 @@ package com.rawatdivyanshu.petclinic.Services;
 
 import com.rawatdivyanshu.petclinic.Models.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet pet);
-
-    Set<Vet> findAll();
 }
