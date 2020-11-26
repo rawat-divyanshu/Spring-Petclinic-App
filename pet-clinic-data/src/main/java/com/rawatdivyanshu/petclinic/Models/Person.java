afb8136ab1e3d657e3163817c4ea5c9823c911dc
Divyanshu Rawat <divyanshu.19r@gmail.com>
2020-11-26 12:17:09 +0530

    Added BaseEntity. Closes #10

--- pet-clinic-data/src/main/java/com/rawatdivyanshu/petclinic/Models/Person.java
+++ pet-clinic-data/src/main/java/com/rawatdivyanshu/petclinic/Models/Person.java
@@ -1,6 +1,6 @@
 package com.rawatdivyanshu.petclinic.Models;
 
-public class Person {
+public class Person extends BaseEntity {
     
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/rawatdivyanshu/petclinic/Models/PetType.java
+++ pet-clinic-data/src/main/java/com/rawatdivyanshu/petclinic/Models/PetType.java
@@ -1,6 +1,6 @@
 package com.rawatdivyanshu.petclinic.Models;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
