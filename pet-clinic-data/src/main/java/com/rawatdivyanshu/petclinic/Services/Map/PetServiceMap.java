f9c98ca18342d32d49fa8c81db0b2605f57e2f22
Divyanshu Rawat <divyanshu.19r@gmail.com>
2020-11-27 11:01:41 +0530

    Implemented Map based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/rawatdivyanshu/petclinic/Services/Map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.rawatdivyanshu.petclinic.Services.Map;
+
+import com.rawatdivyanshu.petclinic.Models.Pet;
+import com.rawatdivyanshu.petclinic.Services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
