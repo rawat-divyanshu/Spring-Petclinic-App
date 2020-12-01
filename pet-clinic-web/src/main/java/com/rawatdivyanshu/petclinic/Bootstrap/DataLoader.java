516c3d29a7a4710d4e786f7fa78cc11564c3a3b9
Divyanshu Rawat <divyanshu.19r@gmail.com>
2020-12-01 10:49:59 +0530

    Refactored Map ID to abstract ID generation. Closes #23

--- pet-clinic-web/src/main/java/com/rawatdivyanshu/petclinic/Bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/rawatdivyanshu/petclinic/Bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Divyanshu");
         owner1.setLastName("Rawat");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Ritika");
         owner2.setLastName("Rawat");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Prithvi");
         vet1.setLastName("Giri");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sanjay");
         vet2.setLastName("Sharma");
 
